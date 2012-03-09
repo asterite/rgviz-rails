4920d4327e5e7ab6b09bdf7df8dc4393dfa756ce
asterite <aborenszweig@manas.com,ar>
2012-03-09 00:16:54 -0300

    Override parse_time and parse_date to use Time.zone from ActiveSupport

--- lib/rgviz_rails.rb
+++ lib/rgviz_rails.rb
@@ -1,8 +1,18 @@
+require 'rgviz'
 require 'rgviz_rails/init.rb'
 require 'rgviz_rails/executor.rb'
 require 'rgviz_rails/js_renderer.rb'
 require 'rgviz_rails/tqx.rb'
 
+class Rgviz::Parser
+  def parse_time(string)
+    Time.zone.parse string
+  end
+
+  def parse_date(string)
+    parse_time(string).to_date
+  end
+end
+
 module RgvizRails
-  
 end
